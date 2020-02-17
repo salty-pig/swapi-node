8fc66d15f116461a54290343bf74abd463c17b6e
Lucas Holmquist <lholmqui@redhat.com>
2020-02-17 16:00:56 -0500

    squash: subresource test

--- test/swapi-subresources-test.js
+++ test/swapi-subresources-test.js
@@ -2,56 +2,51 @@
 
 const nock = require('nock');
 const swapi = require('../lib/swapi-node.js');
-const version = require('../package.json').version;
+const {version} = require('../package.json');
 const test = require('tape');
 
 nock.disableNetConnect();
 
-test('property with link should have a corresponding getter', (t) => {
+test('property with link should have a corresponding getter', async t => {
   nock('https://swapi.co/api/')
     .matchHeader('User-Agent', 'swapi-node')
     .matchHeader('SWAPI-Node-Version', version)
     .get('/people/1')
     .reply(200, {
       name: 'Luke Skywalker',
-      'homeworld': 'https://swapi.co/api/planets/1/',
+      homeworld: 'https://swapi.co/api/planets/1/'
     });
 
-  swapi.getPerson(1).then((result) => {
-    t.equal((typeof result.getHomeworld === 'function'), true, 'should be a function');
+  const result = await swapi.getPerson(1);
+  t.equal((typeof result.getHomeworld === 'function'), true, 'should be a function');
 
-    nock('https://swapi.co/api/')
-      .matchHeader('User-Agent', 'swapi-node')
-      .matchHeader('SWAPI-Node-Version', version)
-      .get('/planets/1/')
-      .reply(200, {
-        'name': 'Tatooine'
-      });
+  nock('https://swapi.co/api/')
+    .matchHeader('User-Agent', 'swapi-node')
+    .matchHeader('SWAPI-Node-Version', version)
+    .get('/planets/1/')
+    .reply(200, {
+      name: 'Tatooine'
+    });
 
-    return result.getHomeworld();
-  }).then(() => {
-    t.ok('succes returned');
-    t.end();
-  });
+  await result.getHomeworld();
+  t.ok('succes returned');
+  t.end();
 });
 
-test('property with out link should have a corresponding getter anyway', (t) => {
-
+test('property with out link should have a corresponding getter anyway', async t => {
   nock('https://swapi.co/api/')
     .matchHeader('User-Agent', 'swapi-node')
     .matchHeader('SWAPI-Node-Version', version)
     .get('/people/1')
     .reply(200, {
       name: 'Luke Skywalker',
-      'homeworld': 'https://swapi.co/api/planets/1/',
+      homeworld: 'https://swapi.co/api/planets/1/'
     });
 
-  swapi.getPerson(1).then((result) => {
-    t.equal((typeof result.getName === 'function'), true, 'should be a function');
+  const result = await swapi.getPerson(1);
+  t.equal((typeof result.getName === 'function'), true, 'should be a function');
 
-    return result.getName();
-  }).then((result) => {
-    t.equal(result, 'Luke Skywalker', 'should equal');
-    t.end();
-  });
+  const personResult = await result.getName();
+  t.equal(personResult, 'Luke Skywalker', 'should equal');
+  t.end();
 });
